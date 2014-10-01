e20b8089f12ff4e3a1f9e5047c54d761e3076f02
Rob Williamson <rob@robwilliamson.com>
2014-10-01 21:54:46 +0200

    Start using instrumentation test cases.

--- data/dbTest/app/src/androidTest/java/com/robwilliamson/dbtest/ApplicationTest.java
+++ data/dbTest/app/src/androidTest/java/com/robwilliamson/dbtest/ApplicationTest.java
@@ -1,13 +1,9 @@
 package com.robwilliamson.dbtest;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<EventActivity> {
     public ApplicationTest() {
-        super(Application.class);
+        super(EventActivity.class);
     }
 }
