73e690924eb820a69dafca4cc5b13fb696e28802
Rob Williamson <rob@robwilliamson.com>
2014-10-01 21:54:21 +0200

    Remove spurious lines.

--- data/dbTest/app/src/main/java/com/robwilliamson/dbtest/EventActivity.java
+++ data/dbTest/app/src/main/java/com/robwilliamson/dbtest/EventActivity.java
@@ -14,7 +14,6 @@ public class EventActivity extends Activity {
         setContentView(R.layout.activity_event);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- data/dbTest/db/src/androidTest/java/com/robwilliamson/db/column/ColumnDefinitionTest.java
+++ data/dbTest/db/src/androidTest/java/com/robwilliamson/db/column/ColumnDefinitionTest.java
@@ -1,7 +1,4 @@
 package com.robwilliamson.db.column;
 
-/**
- * Created by rob on 29/09/14.
- */
 public class ColumnDefinitionTest {
 }
