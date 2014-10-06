3292640c810c7af4cb438e0ccf9cea953a9f01c6
Rob Williamson <rob@robwilliamson.com>
2014-10-06 12:05:13 +0200

    Bring navigation drawer to the front
    Clean up some unused code.

--- data/healthyEsther/app/src/main/java/com/robwilliamson/healthyesther/HomeActivity.java
+++ data/healthyEsther/app/src/main/java/com/robwilliamson/healthyesther/HomeActivity.java
@@ -28,8 +28,6 @@ public class HomeActivity extends FragmentActivity
      */
     private CharSequence mTitle;
 
-    private int mHomeFragmentId;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,38 +44,17 @@ public class HomeActivity extends FragmentActivity
 
         if (findViewById(R.id.drawer_layout) != null) {
             if (savedInstanceState != null) {
-                mHomeFragmentId = savedInstanceState.getInt(FRAGMENT_HOME);
                 return;
             }
 
             HomeFragment homeFragment = new HomeFragment();
             homeFragment.setArguments(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction().add(R.id.drawer_layout, homeFragment).commit();
-            mHomeFragmentId = homeFragment.getId();
+            getSupportFragmentManager().beginTransaction().add(R.id.content_frame, homeFragment).commit();
         }
-    }
-
-    @Override
-    protected void onSaveInstanceState (Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(FRAGMENT_HOME, mHomeFragmentId);
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        //FragmentManager fragmentManager = getFragmentManager();
-        /*fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();*/
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_log_events);
-                break;
-        }
     }
 
     public void restoreActionBar() {
@@ -108,44 +85,4 @@ public class HomeActivity extends FragmentActivity
         int id = item.getItemId();
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_home, container, false);
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((HomeActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
 }
