e7c3495d0ab284c30dc25cfb1997c187ddd2c9a3
Rob Williamson <rob@robwilliamson.com>
2014-10-06 18:04:06 +0200

    Add a conversion factor to get from units to SI
    This should be optional, since some units, like "cupful" may be unknown
    or arbitrary.

--- data/healthyEsther/db/src/main/java/com/robwilliamson/db/definition/Units.java
+++ data/healthyEsther/db/src/main/java/com/robwilliamson/db/definition/Units.java
@@ -8,8 +8,9 @@ import android.database.sqlite.SQLiteDatabase;
  */
 public class Units extends Table {
     public static final String TABLE_NAME = "units";
-    public static final String _Id = "_id";
-    public static final String Name = "name";
+    public static final String _ID = "_id";
+    public static final String NAME = "name";
+    public static final String SI_FACTOR = "si_factor";
 
     @Override
     public String getName() {
@@ -19,9 +20,10 @@ public class Units extends Table {
     @Override
     public void create(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE units ( \n" +
-                "    _id  INTEGER     PRIMARY KEY AUTOINCREMENT,\n" +
-                "    name TEXT( 40 )  NOT NULL\n" +
-                "                     UNIQUE \n" +
+                "    _id       INTEGER     PRIMARY KEY AUTOINCREMENT,\n" +
+                "    name      TEXT( 40 )  NOT NULL\n" +
+                "                          UNIQUE,\n" +
+                "    si_factor REAL \n" +
                 ");\n");
     }
 
