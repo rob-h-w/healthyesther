9a5eff7c9be740388a1d9adc846490b9a5b1e606
Rob Williamson <rob@robwilliamson.com>
2014-10-08 14:41:56 +0200

    Ensure we don't try to change the state of a busy fragment improperly
    Long DB operations would complete after the user had got bored and
    pressed back. We now no longer attempt to modify the state if the instance
    state has already been saved, or hasn't yet been loaded.

--- data/healthyEsther/app/src/main/java/com/robwilliamson/healthyesther/BusyActivity.java
+++ data/healthyEsther/app/src/main/java/com/robwilliamson/healthyesther/BusyActivity.java
@@ -1,15 +1,34 @@
 package com.robwilliamson.healthyesther;
 
+import android.os.Bundle;
+
 import com.robwilliamson.healthyesther.fragment.BusyFragment;
 
 public class BusyActivity extends BaseFragmentActivity {
+    private boolean mBusyIsSettable = false;
     private BusyFragment mBusyFragment;
 
     public BusyActivity() {
         mBusyFragment = new BusyFragment();
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mBusyIsSettable = true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mBusyIsSettable = false;
+    }
+
     protected synchronized void setBusy(boolean busy) {
+        if (!mBusyIsSettable) {
+            return;
+        }
+
         if (busy) {
             mBusyFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction().add(android.R.id.content, mBusyFragment).commit();
