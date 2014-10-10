8b81ecd14c7dcf6bb0dc3e2d28af99fbae194c14
Rob Williamson <rob@robwilliamson.com>
2014-10-10 11:47:21 +0200

    Use the new data generation code.

--- data/healthyEsther/db/src/main/java/com/robwilliamson/db/HealthDbHelper.java
+++ data/healthyEsther/db/src/main/java/com/robwilliamson/db/HealthDbHelper.java
@@ -46,34 +46,11 @@ public final class HealthDbHelper extends SQLiteOpenHelper {
 
     private void fakeData(SQLiteDatabase db) {
         if (/*BuildConfig.DEBUG*/ true) {
-            Contract contract = Contract.getInstance();
-
-            long pieId = contract.MEAL.insert(db, "PAH!");
-            long stewId = contract.MEAL.insert(db, "Stoo");
-
-            long paracetamolId = contract.MEDICATION.insert(db, "Paracetamol");
-
-            for (int i = 0; i < 1000; i++) {
-                int second = (31 * i / 10) % 60;
-                int minute = i % 60;
-                int hour = (127 * i / 10) % 24;
-                int day = (i / 10) % 28 + 1;
-                int type = ((13 * second * hour * i / 10) % 2) + 1;
-                DateTime now = DateTime.now();
-                DateTime then = now.withTime(hour, minute, second, 0);
-                then = then.withDate(2014, 3, day);
-                long eventId = contract.EVENT.insert(db, then, type, "Event " + i);
-
-                if (type == 1) {
-                    contract.MEAL_EVENT.insert(db, (minute % 2 > 0) ? pieId : stewId, eventId);
-                } else {
-                    contract.MEDICATION_EVENT.insert(db, paracetamolId, eventId);
-                }
-            }
+            Utils.Db.TestData.insertFakeData(db);
         }
     }
 
-    private void cleanOldData(SQLiteDatabase db) {
+    public void cleanOldData(SQLiteDatabase db) {
         if (BuildConfig.DEBUG) {
             try {
                 db.beginTransaction();
