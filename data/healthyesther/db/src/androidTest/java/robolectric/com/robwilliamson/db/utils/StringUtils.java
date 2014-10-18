93534bb977943fef4ad6a215c9a9462a1d89da71
Rob Williamson <rob@robwilliamson.com>
2014-10-18 16:33:03 +0200

    Can now run Robolectric tests from command line
    Add instructions. Can't run on IDE unfortunately. It screws the class paths
    every chance it gets.

---
+++ data/healthyesther/db/src/androidTest/java/robolectric/com/robwilliamson/db/utils/StringUtils.java
@@ -0,0 +1,23 @@
+package robolectric.com.robwilliamson.db.utils;
+
+import com.robwilliamson.db.Utils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@Config(manifest = "./src/main/AndroidManifest.xml",
+        emulateSdk=16)
+@RunWith(RobolectricTestRunner.class)
+public class StringUtils {
+    @Test
+    public void testJoin() {
+        String[] list = {"Hello", "world!"};
+        String joined = Utils.join(list, " ");
+        assertThat(joined, equalTo("Hello world!"));
+    }
+}
---
+++ data/healthyesther/readme.txt
@@ -0,0 +1,9 @@
+Can't run Robolectric tests from Android Studio, sadly.
+
+Ensure ./gradlew is executable
+
+chmod +x gradlew
+
+Then, run the unit tests with:
+
+./gradlew test
