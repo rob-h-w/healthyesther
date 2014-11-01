2573ca6c89a292439325b0e9c1ffb61b24ac207f
Rob Williamson <rob@robwilliamson.com>
2014-11-01 23:20:51 +0100

    Test the home activity in both orientations.

--- data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/HomeActivityTest.java
+++ data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/HomeActivityTest.java
@@ -1,9 +1,14 @@
 package com.robwilliamson.healthyesther;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.InstrumentationTestCase;
 
+import com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner;
 import com.robwilliamson.healthyesther.test.HomeActivityAccessor;
 import com.robwilliamson.healthyesther.test.MenuAccessor;
+import com.robwilliamson.healthyesther.test.Orientation;
 
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.*;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
@@ -19,19 +24,37 @@ public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActiv
     protected void setUp() throws Exception {
         super.setUp();
 
+        HomeActivityAccessor.showNavigationDrawer(false, getInstrumentation().getTargetContext());
         getActivity();
     }
 
     public void testActivityContents() {
-        onView(HomeActivityAccessor.healthScoreButton()).check(matches(isClickable()));
-        onView(HomeActivityAccessor.mealScoreButton()).check(matches(isClickable()));
-        onView(HomeActivityAccessor.medicationScoreButton()).check(matches(isClickable()));
+        Orientation.check(new Orientation.Subject() {
+            @Override
+            public InstrumentationTestCase getTestCase() {
+                return HomeActivityTest.this;
+            }
+
+            @Override
+            public void checkContent() {
+                HomeActivityAccessor.checkUnmodifiedContent();
+            }
+        });
     }
 
     public void testMenuContents() {
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 
-        onView(MenuAccessor.backupToDropbox()).check(matches(isEnabled()));
-        onView(MenuAccessor.settings()).check(matches(isEnabled()));
+        Orientation.check(new Orientation.Subject() {
+            @Override
+            public InstrumentationTestCase getTestCase() {
+                return HomeActivityTest.this;
+            }
+
+            @Override
+            public void checkContent() {
+                HomeActivityAccessor.checkMenuContent(getInstrumentation().getTargetContext());
+            }
+        });
     }
 }
--- data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/test/HomeActivityAccessor.java
+++ data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/test/HomeActivityAccessor.java
@@ -1,11 +1,17 @@
 package com.robwilliamson.healthyesther.test;
 
+import android.content.Context;
 import android.view.View;
 
 import com.robwilliamson.healthyesther.R;
 
 import org.hamcrest.Matcher;
 
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isClickable;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isEnabled;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 
 public class HomeActivityAccessor {
@@ -19,5 +25,16 @@ public class HomeActivityAccessor {
 
     public static Matcher<View> medicationScoreButton() {
         return withId(R.id.create_medication_event_button);
+    }
+
+    public static void checkUnmodifiedContent() {
+        onView(healthScoreButton()).check(matches(isClickable()));
+        onView(mealScoreButton()).check(matches(isClickable()));
+        onView(medicationScoreButton()).check(matches(isClickable()));
+    }
+
+    public static void checkMenuContent(Context targetContext) {
+        onView(MenuAccessor.backupToDropbox()).check(matches(isEnabled()));
+        onView(MenuAccessor.settings()).check(matches(isEnabled()));
     }
 }
