6d3d0bbc3537f17b4dbf48c145280c2b70ceb885
Rob Williamson <rob@robwilliamson.com>
2014-11-01 22:51:39 +0100

    Add some Espresso hacks to handle orientation changes.

---
+++ data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/test/Espresso.java
@@ -0,0 +1,73 @@
+package com.robwilliamson.healthyesther.test;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.test.InstrumentationTestCase;
+
+import com.google.android.apps.common.testing.testrunner.ActivityLifecycleMonitor;
+import com.google.android.apps.common.testing.testrunner.GoogleInstrumentation;
+import com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner;
+import com.google.android.apps.common.testing.testrunner.Stage;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Espresso {
+    private static ActivityLifecycleMonitor monitor(GoogleInstrumentationTestRunner instrumentation) {
+        Field lifecycleMonitor;
+        try {
+            lifecycleMonitor = GoogleInstrumentation.class.getDeclaredField("lifecycleMonitor");
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+        lifecycleMonitor.setAccessible(true);
+        try {
+            return (ActivityLifecycleMonitor)lifecycleMonitor.get(instrumentation);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Activity waitForActivityToResume(InstrumentationTestCase testCase) {
+        Collection<Activity> resumed = getResumed(testCase);
+        while(resumed.isEmpty()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            resumed = getResumed(testCase);
+        }
+
+        return (resumed.iterator().next());
+    }
+
+    private static Collection<Activity> getResumed(InstrumentationTestCase testCase) {
+        final Instrumentation instrumentation = testCase.getInstrumentation();
+        final ActivityLifecycleMonitor monitor =  monitor((GoogleInstrumentationTestRunner) instrumentation);
+        final CountDownLatch latch = new CountDownLatch(1);
+        final Object[] resumed = { null };
+
+        try {
+            testCase.runTestOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    resumed[0] = monitor.getActivitiesInStage(Stage.RESUMED);
+                    latch.countDown();
+                }
+            });
+        } catch (Throwable throwable) {
+            return null;
+        }
+
+        try {
+            latch.await(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return (Collection<Activity>) resumed[0];
+    }
+}
---
+++ data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/test/Orientation.java
@@ -0,0 +1,44 @@
+package com.robwilliamson.healthyesther.test;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.pm.ActivityInfo;
+import android.test.InstrumentationTestCase;
+import android.view.View;
+
+import com.google.android.apps.common.testing.testrunner.ActivityLifecycleMonitor;
+import com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner;
+import com.google.android.apps.common.testing.testrunner.Stage;
+
+import java.util.Collection;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+
+public class Orientation {
+    public static interface Subject {
+        InstrumentationTestCase getTestCase();
+        void checkContent();
+    }
+
+    public static void check(Subject subject) {
+        setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, subject);
+        subject.checkContent();
+        setOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE, subject);
+        subject.checkContent();
+    }
+
+    private static void setOrientation(int orientation, Subject subject) {
+        final Activity initial = Espresso.waitForActivityToResume(subject.getTestCase());
+        Activity terminal = null;
+        int initialOrientation = initial.getRequestedOrientation();
+
+        // Only set the orientation if necessary.
+        if (initialOrientation != orientation) {
+            initial.setRequestedOrientation(orientation);
+            terminal = Espresso.waitForActivityToResume(subject.getTestCase());
+        }
+    }
+}
