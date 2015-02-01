1d8f6bad923d036978d48b32325f75d978ed8c60
Rob Williamson <rob@robwilliamson.com>
2015-02-01 14:32:06 +0100

    Add EditScoreEventGroupListAdapter
    This is work towards replacing the EditScoreEventFragments in
    EditScoreEventGroupFragment with a ViewList.

---
+++ data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/adapter/EditScoreEventGroupListAdapter.java
@@ -0,0 +1,55 @@
+package com.robwilliamson.healthyesther.adapter;
+
+import android.app.Activity;
+import android.support.v4.util.Pair;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.robwilliamson.db.definition.HealthScore;
+import com.robwilliamson.healthyesther.R;
+import com.robwilliamson.healthyesther.Utils;
+
+import java.util.List;
+
+public class EditScoreEventGroupListAdapter extends OptimizedListAdapter<EditScoreEventGroupListAdapter.Tag, View, Pair<HealthScore.Score, Integer>> {
+
+    protected static class Tag {
+        public TextView title;
+        public TextView minLabel;
+        public TextView maxLabel;
+        public RatingBar ratingBar;
+        public LinearLayout layout;
+    }
+
+    public EditScoreEventGroupListAdapter(Activity context) {
+        super(context, R.layout.fragment_edit_score_event);
+    }
+
+    public EditScoreEventGroupListAdapter(Activity context, List<Pair<HealthScore.Score, Integer>> list) {
+        super(context, R.layout.fragment_edit_score_event, list);
+    }
+
+    @Override
+    protected Tag getTagFor(final View view) {
+        return new Tag() {
+            {
+                title = Utils.View.getTypeSafeView(view, R.id.score_name_title);
+                minLabel = Utils.View.getTypeSafeView(view, R.id.score_minimum_label);
+                maxLabel = Utils.View.getTypeSafeView(view, R.id.score_maximum_label);
+                ratingBar = Utils.View.getTypeSafeView(view, R.id.score_bar);
+                layout = Utils.View.getTypeSafeView(view, R.id.edit_score_layout);
+            }
+        };
+    }
+
+    @Override
+    protected void populateTag(Tag tag, Pair<HealthScore.Score, Integer> data) {
+        tag.title.setText(data.first.name);
+        tag.minLabel.setText(data.first.minLabel);
+        tag.maxLabel.setText(data.first.maxLabel);
+        tag.ratingBar.setMax(HealthScore.MAX);
+        tag.ratingBar.setRating(data.second);
+    }
+}
