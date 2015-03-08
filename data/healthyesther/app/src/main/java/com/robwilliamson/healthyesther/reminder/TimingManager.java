805f873936e0fb78c1357331b99aaa56f052364e
Rob Williamson <rob@robwilliamson.com>
2015-03-08 21:58:11 +0100

    Fix the timing to do notifications from 8:00 to 22:00.

--- data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/reminder/TimingManager.java
+++ data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/reminder/TimingManager.java
@@ -7,9 +7,15 @@ import android.content.Intent;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.Interval;
+import org.joda.time.Period;
 
 public enum TimingManager {
     INSTANCE;
+
+    private static final int MILLISECONDS_IN_SECOND = 1000;
+    private static final int SECONDS_IN_MINUTE = 60;
+    private static final int MINUTES_IN_HOUR = 60;
 
     public void next(Context context) {
         AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
@@ -24,6 +30,23 @@ public enum TimingManager {
     }
 
     private long getMillis() {
-        return 5000;
+        DateTime now = DateTime.now().withZone(DateTimeZone.getDefault());
+        DateTime earliest = DateTime.now().withHourOfDay(8).withMinuteOfHour(0);
+        DateTime latest = DateTime.now().withHourOfDay(22).withMinuteOfHour(0);
+        long period = 2 * MINUTES_IN_HOUR * SECONDS_IN_MINUTE * MILLISECONDS_IN_SECOND;
+
+        long next = period;
+
+        if (now.isAfter(latest)) {
+            // Move earliest until tomorrow.
+            earliest = earliest.plus(Period.days(1));
+        }
+
+        if (now.isBefore(earliest) || now.isAfter(latest)) {
+            Interval waitInterval = new Interval(now, earliest);
+            period = waitInterval.getEndMillis();
+        }
+
+        return next;
     }
 }
