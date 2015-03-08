cd666a1ba398bf732f001c046d172cf21a3d34d1
Rob Williamson <rob@robwilliamson.com>
2015-03-08 21:58:22 +0100

    Optimize an import.

--- data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/reminder/ReminderIntentService.java
+++ data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/reminder/ReminderIntentService.java
@@ -4,8 +4,8 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
 
 import com.robwilliamson.healthyesther.HomeActivity;
 import com.robwilliamson.healthyesther.R;
