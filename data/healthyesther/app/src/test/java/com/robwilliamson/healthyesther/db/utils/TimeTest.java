14c85049c142f6df6093dbb55c7a525a0287080e
Rob Williamson <rob@robwilliamson.com>
2015-03-21 20:41:18 +0100

    Update Robolectric to 2.4
    Also include Robolectric into the app project.

---
+++ data/healthyesther/app/src/test/java/com/robwilliamson/healthyesther/db/utils/TimeTest.java
@@ -0,0 +1,15 @@
+package com.robwilliamson.db.utils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 18)
+public class TimeTest {
+    @Test
+    public void testBundling() {
+        //
+    }
+}
