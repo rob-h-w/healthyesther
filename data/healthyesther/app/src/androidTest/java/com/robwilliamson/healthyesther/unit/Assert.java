a64f0630f2b20255e812c592ebc2556a99cbed71
Rob Williamson <rob@robwilliamson.com>
2015-03-22 12:07:11 +0100

    Add a DateTime assertion with an accuracy value.

--- data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/unit/Assert.java
+++ data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/unit/Assert.java
@@ -1,10 +1,21 @@
 package com.robwilliamson.healthyesther.unit;
 
+import com.robwilliamson.healthyesther.util.time.Range;
+
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
+
+import static junit.framework.Assert.assertTrue;
 
 public final class Assert {
     public static void assertIsEqual(DateTime expected, DateTime actual) {
-        junit.framework.Assert.assertTrue("Expected time " + expected +
+        assertTrue("Expected time " + expected +
                 " is equal to actual time " + actual, expected.isEqual(actual));
+    }
+
+    public static void assertIsEqual(DateTime expected, Duration sigma, DateTime actual) {
+        Range range = new Range(expected, sigma);
+        assertTrue("Expected time to be about " + expected + ", actually was " + actual,
+                range.contains(actual));
     }
 }
