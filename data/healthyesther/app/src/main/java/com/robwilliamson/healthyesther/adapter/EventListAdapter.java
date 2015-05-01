f118d30cc25ea896952b46add763882959f606e7
Rob Williamson <rob@robwilliamson.com>
2015-05-01 09:23:27 +0200

    Add an event list adapter
    Use Views instead of fragments in the list.

---
+++ data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/adapter/EventListAdapter.java
@@ -0,0 +1,81 @@
+package com.robwilliamson.healthyesther.adapter;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.robwilliamson.healthyesther.R;
+import com.robwilliamson.healthyesther.Utils;
+import com.robwilliamson.healthyesther.db.definition.Event;
+
+import java.util.List;
+
+public class EventListAdapter extends OptimizedListAdapter<EventListAdapter.Tag, View, Event.Value> {
+    protected static class Tag {
+        public ImageView eventIcon;
+        public TextView title;
+        public TextView time;
+    }
+
+    public EventListAdapter(Activity context, int parentViewId) {
+        super(context, parentViewId);
+    }
+
+    public EventListAdapter(Activity context, int parentViewId, List<Event.Value> data) {
+        super(context, parentViewId, data);
+    }
+
+    /**
+     * Get a tag for a view. A tag should contain references to every view modified when new data is
+     * used.
+     *
+     * @param view The view requiring a tag.
+     * @return A new tag for the view.
+     */
+    @Override
+    protected Tag getTagFor(final View view) {
+        return new Tag() {
+            {
+                eventIcon = Utils.View.getTypeSafeView(view, R.id.eventIcon);
+                title = Utils.View.getTypeSafeView(view, R.id.eventTitle);
+                time = Utils.View.getTypeSafeView(view, R.id.eventTime);
+            }
+        };
+    }
+
+    /**
+     * Use a tag to populate the features of a view. The tag will have been returned by
+     * {@link #getTagFor(View view)}.
+     *
+     * @param tag  The tag to use with the view.
+     * @param data The data to populate the tag's views with.
+     */
+    @Override
+    protected void populateTag(Tag tag, Event.Value data) {
+        int iconId = 0;
+        switch (Event.Type.valueOf(data.typeId)) {
+            case MEAL:
+                iconId = R.drawable.ic_food;
+                break;
+            case MEDICATION:
+                iconId = R.mipmap.ic_medication;
+                break;
+            case HEALTH_SCORE:
+                iconId = R.mipmap.ic_health;
+                break;
+            case NOTE:
+                iconId = R.mipmap.ic_note;
+                break;
+            default:
+                iconId = R.drawable.ic_launcher;
+                break;
+        }
+
+
+        tag.eventIcon.setImageResource(iconId);
+
+        tag.title.setText(data.name);
+        tag.time.setText(data.when.toString());
+    }
+}
