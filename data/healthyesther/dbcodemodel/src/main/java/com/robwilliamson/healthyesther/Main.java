a3e0e6c45e1b71ecb83e4900f151febdf8fbcc60
Rob Williamson <rob@robwilliamson.com>
2015-06-12 11:23:11 +0200

    Add arguments for the source generation tool
    Just very simple implementation for now.

--- data/healthyesther/dbcodemodel/src/main/java/com/robwilliamson/healthyesther/Main.java
+++ data/healthyesther/dbcodemodel/src/main/java/com/robwilliamson/healthyesther/Main.java
@@ -1,7 +1,40 @@
 package com.robwilliamson.healthyesther;
 
 public final class Main {
+    private static final int ARG_COUNT = 3;
+    private static final String USAGE =
+            "<Source Folder> <Destination Folder> <Destination Package>";
+
     public static void main(String[] args) {
+        if (args.length < ARG_COUNT) {
+            System.err.printf("\nExpected %d arguments, %d received.\n", ARG_COUNT, args.length);
+            printUsage();
+            return;
+        }
+
+        if (!valuePresent(args[0], "Source Folder") ||
+                !valuePresent(args[1], "Destination Folder") ||
+                !valuePresent(args[2], "Destination Package")) {
+            printUsage();
+            return;
+        }
+
+        String sourceFolder = args[0];
+        String destinationFolder = args[1];
+        String destinationPackage = args[2];
+
         System.out.printf("hello world");
+    }
+
+    private static void printUsage() {
+        System.out.println(USAGE);
+    }
+
+    private static boolean valuePresent(String value, String name) {
+        if (value == null || value.isEmpty()) {
+            System.err.printf("\n%s was not provided.\n", name);
+            return false;
+        }
+        return true;
     }
 }
