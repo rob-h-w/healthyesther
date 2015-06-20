84e9d9d9c2cf8816292695a7906c86396f3d4a39
Rob Williamson <rob@robwilliamson.com>
2015-06-20 22:35:20 +0200

    Rename the BaseTable type to Table.

--- data/healthyesther/dbcodemodel/src/main/java/com/robwilliamson/healthyesther/generator/BaseTable.java
+++ data/healthyesther/dbcodemodel/src/main/java/com/robwilliamson/healthyesther/generator/BaseTable.java
@@ -4,7 +4,7 @@ import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JMod;
 import com.sun.codemodel.JPackage;
 
-@ClassGeneratorFeatures(name = "BaseTable", parameterName = "table")
+@ClassGeneratorFeatures(name = "Table", parameterName = "table")
 public class BaseTable extends BaseClassGenerator {
     public BaseTable(JPackage jPackage) throws JClassAlreadyExistsException {
         setJClass(jPackage._class(JMod.PUBLIC | JMod.ABSTRACT, getName()));
