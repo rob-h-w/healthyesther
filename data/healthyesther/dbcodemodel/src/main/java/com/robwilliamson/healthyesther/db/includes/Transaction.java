6c739c676a8f16ff634e906a8b5c15c62cf69017
Rob Williamson <rob@robwilliamson.com>
2015-09-01 11:52:56 +0200

    Add execSQL to Transaction interface
    To enable creation of the database & tables.

--- data/healthyesther/dbcodemodel/src/main/java/com/robwilliamson/healthyesther/db/includes/Transaction.java
+++ data/healthyesther/dbcodemodel/src/main/java/com/robwilliamson/healthyesther/db/includes/Transaction.java
@@ -5,6 +5,9 @@ import java.util.List;
 public interface Transaction {
     public void addCompletionHandler(CompletionHandler handler);
 
+
+    public String execSQL(String sql);
+
     public long insert(List<String> columnNames, Object... columnValues);
 
     public static interface CompletionHandler {
