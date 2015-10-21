0fc08bdb7210fff8a83a26296a24226ccbc96372
Rob Williamson <rob@robwilliamson.com>
2015-10-21 23:48:00 +0200

    Enable unit testing using Robolectric.

--- data/healthyesther/app/src/test/java/com/robwilliamson/healthyesther/reminder/TimingManagerTest.java
+++ data/healthyesther/app/src/test/java/com/robwilliamson/healthyesther/reminder/TimingManagerTest.java
@@ -1,14 +1,16 @@
 package com.robwilliamson.healthyesther.reminder;
 
+import com.robwilliamson.healthyesther.BuildConfig;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertTrue;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 18)
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class TimingManagerTest {
     @Test
     public void testInstantiation() throws Exception {
