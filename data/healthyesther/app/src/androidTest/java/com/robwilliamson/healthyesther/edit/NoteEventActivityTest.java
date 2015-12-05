9ad744dbff37af365202831bbd33bfc69de6856a
Rob Williamson <rob@robwilliamson.com>
2015-12-05 18:42:11 +0100

    Check that notes are stored correctly.

--- data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/edit/NoteEventActivityTest.java
+++ data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/edit/NoteEventActivityTest.java
@@ -3,33 +3,53 @@ package com.robwilliamson.healthyesther.edit;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.InstrumentationTestCase;
 
+import com.robwilliamson.healthyesther.HomeActivity;
 import com.robwilliamson.healthyesther.Settings;
+import com.robwilliamson.healthyesther.db.HealthDbHelper;
 import com.robwilliamson.healthyesther.db.Utils;
+import com.robwilliamson.healthyesther.db.generated.NoteTable;
+import com.robwilliamson.healthyesther.db.includes.Database;
+import com.robwilliamson.healthyesther.db.includes.WhereContains;
+import com.robwilliamson.healthyesther.db.integration.DatabaseAccessor;
 import com.robwilliamson.healthyesther.test.EditEventAccessor;
+import com.robwilliamson.healthyesther.test.HomeActivityAccessor;
 import com.robwilliamson.healthyesther.test.NoteEventActivityAccessor;
 import com.robwilliamson.healthyesther.test.Orientation;
 
+import javax.annotation.Nullable;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 
-public class NoteEventActivityTest extends ActivityInstrumentationTestCase2<NoteEventActivity> {
+public class NoteEventActivityTest extends ActivityInstrumentationTestCase2<HomeActivity> {
     public NoteEventActivityTest() {
-        super(NoteEventActivity.class);
+        super(HomeActivity.class);
     }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
 
+        HomeActivityAccessor.setShowNavigationDrawer(false, getInstrumentation().getTargetContext());
+
         Utils.Db.TestData.cleanOldData();
         Settings.INSTANCE.resetExclusionList();
 
         getActivity();
+
+        HomeActivityAccessor.AddMode.start();
     }
 
     public void testOpenNoteActivity() {
+        onView(HomeActivityAccessor.AddMode.noteButton()).perform(click());
         Orientation.check(new Orientation.Subject() {
             @Override
             public InstrumentationTestCase getTestCase() {
@@ -44,6 +64,7 @@ public class NoteEventActivityTest extends ActivityInstrumentationTestCase2<Note
     }
 
     public void testTextRetention() {
+        onView(HomeActivityAccessor.AddMode.noteButton()).perform(click());
         final String title = "note title";
         onView(NoteEventActivityAccessor.nameValue()).perform(typeText(title));
         Orientation.check(new Orientation.Subject() {
@@ -60,9 +81,19 @@ public class NoteEventActivityTest extends ActivityInstrumentationTestCase2<Note
     }
 
     public void test_addNoteName_updatesEventName() {
+        onView(HomeActivityAccessor.AddMode.noteButton()).perform(click());
         String noteName = "A Note";
         onView(NoteEventActivityAccessor.nameValue()).perform(typeText(noteName));
 
         onView(EditEventAccessor.eventEditText()).check(matches(withText(noteName)));
+    }
+
+    private void checkDatabaseCorrectness(@Nullable String name) {
+        onView(HomeActivityAccessor.AddMode.noteButton()).check(matches(isDisplayed()));
+
+        Database db = HealthDbHelper.getDatabase();
+        NoteTable.Row row = DatabaseAccessor.NOTE_TABLE.select0Or1(db, WhereContains.columnEqualling(NoteTable.NAME, name));
+
+        assertThat(row, not(is((NoteTable.Row) nullValue())));
     }
 }
