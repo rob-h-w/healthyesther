7bfeb60c6497ba3e7e473ebac255a7724f2bdf34
Rob Williamson <rob@robwilliamson.com>
2015-12-06 11:33:00 +0100

    Make medication UI tests more granular, & expand coverage.

--- data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/edit/MedicationEventActivityTest.java
+++ data/healthyesther/app/src/androidTest/java/com/robwilliamson/healthyesther/edit/MedicationEventActivityTest.java
@@ -6,23 +6,33 @@ import android.test.InstrumentationTestCase;
 
 import com.robwilliamson.healthyesther.HomeActivity;
 import com.robwilliamson.healthyesther.R;
+import com.robwilliamson.healthyesther.db.HealthDbHelper;
 import com.robwilliamson.healthyesther.db.Utils;
+import com.robwilliamson.healthyesther.db.generated.MedicationTable;
+import com.robwilliamson.healthyesther.db.includes.Database;
+import com.robwilliamson.healthyesther.db.includes.WhereContains;
+import com.robwilliamson.healthyesther.db.integration.DatabaseAccessor;
 import com.robwilliamson.healthyesther.test.EditEventAccessor;
 import com.robwilliamson.healthyesther.test.HomeActivityAccessor;
-import com.robwilliamson.healthyesther.test.MealEventActivityAccessor;
 import com.robwilliamson.healthyesther.test.MedicationEventActivityAccessor;
 import com.robwilliamson.healthyesther.test.Orientation;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.robwilliamson.healthyesther.test.Strings.from;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 public class MedicationEventActivityTest extends ActivityInstrumentationTestCase2<HomeActivity> {
+    private static final String MED_NAME = "Paracetamol";
+
     public MedicationEventActivityTest() {
         super(HomeActivity.class);
     }
@@ -40,7 +50,13 @@ public class MedicationEventActivityTest extends ActivityInstrumentationTestCase
         HomeActivityAccessor.AddMode.start();
     }
 
-    public void testAddNewMeal() {
+    public void testOkAbsentWhenNameEmpty() {
+        onView(HomeActivityAccessor.AddMode.medicationScoreButton()).perform(click());
+
+        onView(EditEventAccessor.ok()).check(doesNotExist());
+    }
+
+    public void testUiInBothOrientations() {
         onView(HomeActivityAccessor.AddMode.medicationScoreButton()).perform(click());
 
         Orientation.check(new Orientation.Subject() {
@@ -55,9 +71,12 @@ public class MedicationEventActivityTest extends ActivityInstrumentationTestCase
                 onView(MedicationEventActivityAccessor.medName()).check(matches(withHint(from(R.string.enter_medication_name))));
             }
         });
+    }
 
-        final String medName = "Paracetamol";
-        onView(MedicationEventActivityAccessor.medName()).perform(typeText(medName));
+    public void testTextPersistence() {
+        onView(HomeActivityAccessor.AddMode.medicationScoreButton()).perform(click());
+
+        onView(MedicationEventActivityAccessor.medName()).perform(typeText(MED_NAME));
 
         Orientation.check(new Orientation.Subject() {
             @Override
@@ -67,10 +86,23 @@ public class MedicationEventActivityTest extends ActivityInstrumentationTestCase
 
             @Override
             public void checkContent() {
-                onView(MedicationEventActivityAccessor.medName()).check(matches(withText(medName)));
+                onView(MedicationEventActivityAccessor.medName()).check(matches(withText(MED_NAME)));
+                onView(EditEventAccessor.eventEditText()).check(matches(withText(MED_NAME)));
             }
         });
+    }
+
+    public void testAddNewMed() {
+        onView(HomeActivityAccessor.AddMode.medicationScoreButton()).perform(click());
+
+        onView(MedicationEventActivityAccessor.medName()).perform(typeText(MED_NAME));
 
         onView(EditEventAccessor.ok()).perform(click());
+
+        Database db = HealthDbHelper.getDatabase();
+        MedicationTable.Row medication = DatabaseAccessor.MEDICATION_TABLE.select0Or1(db, WhereContains.all());
+        assertThat(medication, not(is((MedicationTable.Row) null)));
+        //noinspection ConstantConditions
+        assertThat(medication.getName(), is(MED_NAME));
     }
 }
