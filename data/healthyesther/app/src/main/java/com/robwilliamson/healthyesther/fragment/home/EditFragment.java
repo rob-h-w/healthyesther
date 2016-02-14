7add62f40cac0ce8083529a4c6b26ffe935d4466
Rob Williamson <rob@robwilliamson.com>
2016-02-14 14:20:32 +0100

    Remove unused method.

--- data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/fragment/home/EditFragment.java
+++ data/healthyesther/app/src/main/java/com/robwilliamson/healthyesther/fragment/home/EditFragment.java
@@ -1,17 +1,11 @@
 package com.robwilliamson.healthyesther.fragment.home;
 
 import com.robwilliamson.healthyesther.R;
-import com.robwilliamson.healthyesther.Utils;
 import com.robwilliamson.healthyesther.fragment.DbFragment;
-import com.robwilliamson.healthyesther.fragment.edit.EventHistoryListFragment;
 
 public class EditFragment extends DbFragment {
     @Override
     protected int getFragmentLayout() {
         return R.layout.fragment_home_edit;
-    }
-
-    private EventHistoryListFragment getEventHistoryFragment() {
-        return Utils.View.getTypeSafeFragment(getChildFragmentManager(), R.id.event_list_fragment, EventHistoryListFragment.class);
     }
 }
